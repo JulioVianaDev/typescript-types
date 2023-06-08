744a21514d0faaddbc48a9d38e747fe197fdda7d
juliovianadev <juliovianadev@gmail.com>
2023-06-08 15:52:59 -0300

    lists

--- basics.ts
+++ basics.ts
@@ -1,8 +1,10 @@
 const person: {
     name: string,
     age: number,
+    amigos:string[]
 } ={
     name: "oi",
-    age: 12
+    age: 12,
+    amigos: ["oi","tudo","bem"]
 }
 console.log(person.name)
