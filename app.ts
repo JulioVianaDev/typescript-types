6888604ee0697634c4b4bcfdd6589472a9a2db6a
juliovianadev <juliovianadev@gmail.com>
2023-06-08 15:40:06 -0300

    simple sum function

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('Your code goes here...');
+function add(n1:number,n2:number){
+    return n1+n2;
+}
+
+const number1 = 12
+const number2 = 13
+
+const result =add(number1,number2)
+console.log(result)
